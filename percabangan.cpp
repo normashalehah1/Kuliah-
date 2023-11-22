d08176b8273038bdbf83fb249fed4d4e2fe6ea37
Norma <99189427+normashalehah1@users.noreply.github.com>
2023-11-22 21:13:41 +0800

    Create percabangan.cpp

---
+++ percabangan.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+  int x;
+  
+  cout << "masukan nilai x: ";
+  cin >> x;
+  
+  if (x>10) {
+  cout << "x lebih besar dari 10" <<endl;
+  } else {
+  cout << "x kurang dari atau sama dengan 10" <<endl;
+  }
+}
+    
